e9ea7af19ee700aa37903f395f1ffc0c5abc3fa8
ozankenangungor <s220204044@ankarabilim.edu.tr>
2024-11-28 00:51:53 +0300

    Initial Screen

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,8 @@ import { ImageBackground, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import beachImage from "@/assets/meditation-images/beach.webp";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-const image = { uri: "https://legacy.reactjs.org/logo-og.png" };
-
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
 const App = () => (
   <View className="flex-1">
     <ImageBackground source={beachImage} resizeMode="cover" className="flex-1">
@@ -14,10 +13,24 @@ const App = () => (
         className="flex-1"
         colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
       >
-        <SafeAreaView>
-          <Text className="text-center text-white font-bold text-4xl">
-            Simple Meditation
-          </Text>
+        <SafeAreaView className="flex-1 px-1 justify-between">
+          <View>
+            <Text className="text-center text-white font-bold text-4xl">
+              Simple Meditation
+            </Text>
+            <Text className="text-center text-white text-2xl mt-3">
+              Simplifying Meditation for Everyone
+            </Text>
+          </View>
+
+          <View>
+            <CustomButton
+              onPress={() => console.log("click")}
+              title="Get Started"
+            />
+          </View>
+
+          <StatusBar style="light" />
         </SafeAreaView>
       </LinearGradient>
     </ImageBackground>
